a713ffa6b8596b181d7634f4ef4bafc9b5fd1477
abeidahmed <abeidahmed92@gmail.com>
2021-03-14 12:46:43 +0530

    Implement logout functionality in app Header component

--- app/application/components/Header.tsx
+++ app/application/components/Header.tsx
@@ -1,5 +1,23 @@
+import { useMutation, useRouter } from "blitz"
+import logout from "app/auth/mutations/logout"
+
 const Header = () => {
-  return <div>hello world from header</div>
+  const [logoutMutation] = useMutation(logout)
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    await logoutMutation()
+    await router.push("/")
+  }
+
+  return (
+    <div>
+      <h1>hello world</h1>
+      <button type="button" onClick={handleLogout}>
+        Log out
+      </button>
+    </div>
+  )
 }
 
 export default Header
