02b75f83ddeb8a116686d1e63c5166495a542dca
abeidahmed <abeidahmed92@gmail.com>
2021-03-14 12:17:41 +0530

    Initialize project migration

---
+++ db/migrations/20210314064711_add_project/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL DEFAULT E'#4B5563',
+
+    PRIMARY KEY ("id")
+);
